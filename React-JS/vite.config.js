65ca8843b538718130563472c0e76ea0febbae2a
ZAKARIASAKAMI <zakariasakami4@gmail.com>
2026-06-20 18:10:08 +0100

    Update-after-login

--- React-JS/vite.config.js
+++ React-JS/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
-// https://vite.dev/config/
+// https://vite.dev/config
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
